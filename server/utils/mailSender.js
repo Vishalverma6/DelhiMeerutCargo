d85e4effb372100a4cc7845db25dc53a2c64b239
Vishalverma6 <Vishalverma67600@gmail.com>
2026-06-28 16:37:52 +0530

    updating the mailsender

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -6,13 +6,16 @@ const mailSender = async (email, title, body) => {
     try {
         let transporter = nodemailer.createTransport({
             host: process.env.MAIL_HOST,
-            port: Number(process.env.MAIL_PORT),
+            port: process.env.MAIL_PORT,
             secure: true, // Required for port 465
+            family: 4,
             auth: {
                 user: process.env.MAIL_USER,
                 pass: process.env.MAIL_PASS,
             }
         })
+        await transporter.verify();
+        console.log("SMTP Connected");
 
         let info = await transporter.sendMail({
             from: `"Delhi Meerut Cargo" <${process.env.MAIL_USER}>`,
@@ -20,7 +23,7 @@ const mailSender = async (email, title, body) => {
             subject: `${title}`,
             html: `${body}`,
         })
-        console.log("info", info);
+        // console.log("info", info);
         return info;
     } catch (error) {
         console.log(error.message);
